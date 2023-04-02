c729eed138b8f577301907c8839511c0a8f189a3
Tathagat Negi <tarangnegi299@gmail.com>
2023-04-02 18:50:32 +0530

    added personal config files

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,5 @@
+--TODO: Test
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +11,21 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- -- Lua
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    -- config = function()
+    --   require("todo-comments").setup {
+    --     -- your configuration comes here
+    --     -- or leave it empty to use the default settings
+    --     -- refer to the configuration section below
+    --   }
+    opts = {},
+    event = "User Astrofile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>" },
+    },
+  },
 }
